4372436e19f7d2020300229fd2240c07f247f2d5
blandschwad <blandschwad@gmail.com>
2024-04-11 09:01:14 -0700

    session provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { MantineProvider, createEmotionCache } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import type { AppProps } from "next/app";
 import { Noto_Sans_JP } from "next/font/google";
-
+import {SessionProvider} from "next-auth/react"
 const appendCache = createEmotionCache({ key: "mantine", prepend: false });
 
 const notosams = Noto_Sans_JP({
@@ -12,21 +12,24 @@ const notosams = Noto_Sans_JP({
   display: "swap",
 });
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      emotionCache={appendCache}
-      theme={{
-        colorScheme: "light",
-        fontFamily: notosams.style.fontFamily,
-        fontFamilyMonospace: notosams.style.fontFamily,
-        headings: { fontFamily: notosams.style.fontFamily },
-      }}
-    >
-      <Notifications />
-      <Component {...pageProps} />
-    </MantineProvider>
+    <SessionProvider session={session}>
+      <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            emotionCache={appendCache}
+            theme={{
+              colorScheme: "dark",
+              fontFamily: notosams.style.fontFamily,
+              fontFamilyMonospace: notosams.style.fontFamily,
+              headings: { fontFamily: notosams.style.fontFamily },
+            }}
+          >
+            <Notifications />
+            <Component {...pageProps} />
+          </MantineProvider>
+    </SessionProvider>
+    
   );
 }
