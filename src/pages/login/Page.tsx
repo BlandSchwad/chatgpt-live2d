df8eaaaed6ed90258551379f4255a6f519c9d621
blandschwad <blandschwad@gmail.com>
2024-04-12 14:53:24 -0700

    front end login redirect and session

--- src/pages/login/Page.tsx
+++ src/pages/login/Page.tsx
@@ -1,7 +1,14 @@
 import { LoginPageView } from "./View";
 import type { NextPage } from "next";
+// import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export const LoginPage: NextPage = () => {
+  const session = useSession()
+  // const router = useRouter()
+  // if(session){ 
+  //   router.push('/home')
+  // }
   return (
     <>
       <LoginPageView />
